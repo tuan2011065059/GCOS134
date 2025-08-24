aa3cb7b923e849168897716ec1b0b48b3956dbc6
tuan2011065059 <tuanlmht12002@gmail.com>
2025-08-24 14:40:57 +0700

    update

--- app.py
+++ app.py
@@ -9,3 +9,30 @@ app.secret_key = os.environ.get("SECRET_KEY", "ntnqplnvtc")
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("agent_movement.html")
+
+@app.route("/agent-info")
+def agent_info():
+    return render_template("tree.html")  
+
+@app.route("/agent-movement")
+def agent_movement():
+    return render_template("agent_movement.html")
+
+@app.route("/commission")
+def commission():
+    return render_template("commission.html")
+
+@app.route("/monthly-bonus")
+def monthly_bonus():
+    return render_template("monthly.html")
